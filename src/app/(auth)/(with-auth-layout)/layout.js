61b77ac14a1db571385f62b6ad191b076df907e7
Charles Hsiao <Charles310@gmail.com>
2024-10-24 15:35:18 +0800

    Group Layout說明

---
+++ src/app/(auth)/(with-auth-layout)/layout.js
@@ -0,0 +1,6 @@
+export default function AuthLayout({children}){
+    return <div>
+        <h2>Inner layout</h2>
+        {children}
+    </div>
+}
---
+++ src/app/products/[productid]/layout.js
@@ -0,0 +1,6 @@
+export default function ProductDetailsLayout({children}){
+    return <>
+    {children}
+    <h2>Layout Features products</h2>
+    </>
+}
